9ce2bc039c7b592a4dfd95bb8ec6fcc8e78a3f36
AdithyaRusith <129499315+adith23@users.noreply.github.com>
2025-02-08 22:33:02 +0530

    Updated Design

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/book.dart';
 
-/// BookList widget
-/// Uses the [Book] widget to display a list of books.
 class BookList extends StatelessWidget {
   const BookList({super.key});
 
@@ -11,31 +9,155 @@ class BookList extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Ceylon Bookstore'),
-          backgroundColor: Colors.blueGrey,
+          title: const Padding(
+            padding: EdgeInsets.only(left: 5), // Added left padding
+            child: Text(
+              'Inkwell Books',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 147, 145, 255),
         ),
-        body: SafeArea(   
-          child: ListView(
-            children: const [
-              Book(
-                coverImagePath: 'assets/images/images.jpg',
-                title: 'Rich Dad, Poor Dad',
-                author: 'Robert T. Kiyosaki',
-                price: 1250,
-              ),
-              Book(
-                coverImagePath: 'assets/images/images1.jpg',
-                title: 'The Alchemist',
-                author: 'Coelho Paulo',
-                price: 1000,
-              ),
-              Book(
-                coverImagePath: 'assets/images/images2.jpg',
-                title: 'Men Are from Mars',
-                author: 'John Gray',
-                price: 1350,
-              ),
-            ],
+        backgroundColor: const Color.fromARGB(255, 236, 220, 255),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, top: 15, bottom: 10),
+                  child: Text(
+                    'Popular Books',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 340,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      Book(
+                        coverImagePath: 'assets/images/images8.jpg',
+                        title: 'Shoe Dog',
+                        author: 'Phil Knight',
+                        price: 1350,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images5.jpg',
+                        title: 'Thinking, Fast and Slow',
+                        author: 'Daniel Kahneman ',
+                        price: 1250,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images7.jpg',
+                        title: 'The Richest Man in Babylon',
+                        author: 'George S. Clason',
+                        price: 1000,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images2.jpg',
+                        title: 'Men are from mars women are from venus',
+                        author: 'John Gray',
+                        price: 1500,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images1.jpg',
+                        title: 'The Alchemist',
+                        author: ' Paulo Coelho',
+                        price: 1800,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images3.jpg',
+                        title: 'The Psychology of Money',
+                        author: 'Morgan Housel',
+                        price: 1600,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images.jpg',
+                        title: 'Rich Dad and Poor Dad',
+                        author: 'Robert T. Kiyosaki',
+                        price: 1700,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images4.jpg',
+                        title: '48 Laws of Power',
+                        author: 'Robert Greene',
+                        price: 1900,
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, top: 15, bottom: 10),
+                  child: Text(
+                    'New Releases',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 340,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      Book(
+                        coverImagePath: 'assets/images/images9.jpg',
+                        title: 'Onyx Storm',
+                        author: 'Rebecca Yarros',
+                        price: 1800,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images10.jpg',
+                        title: 'How to Win at Travel',
+                        author: 'Brian Kelly ',
+                        price: 2000,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images11.jpg',
+                        title: 'When the Moon Hatched',
+                        author: 'Sarah A. Parker',
+                        price: 1500,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images12.jpg',
+                        title: 'North Is the Night',
+                        author: 'Emily Rath',
+                        price: 1800,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images13.jpg',
+                        title: 'Open Socrates',
+                        author: 'Agnes Callard',
+                        price: 2000,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images14.jpg',
+                        title: 'The Coming Wave ',
+                        author: 'Mustafa Suleyman ',
+                        price: 1500,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images15.jpg',
+                        title: 'The AI Revolution: Transforming Manufacturing',
+                        author: 'Dean H. Stanton',
+                        price: 1800,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images16.jpg',
+                        title: 'Machine Learning System Design Interview',
+                        author: 'Ali Aminian',
+                        price: 2000,
+                      ),
+                      Book(
+                        coverImagePath: 'assets/images/images17.jpg',
+                        title: 'Source Code: My Beginnings',
+                        author: 'Bill Gates',
+                        price: 1500,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/book.dart
+++ lib/widgets/book.dart
@@ -16,51 +16,71 @@ class Book extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            Text(
-              author,
-              style: const TextStyle(
-                fontStyle: FontStyle.italic,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Image.asset(
-              coverImagePath,
-              width: 140,
-              height: 180,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Text(
-                  'Price: $price',
-                  style: const TextStyle(fontSize: 16),
+    return SizedBox(
+      width: 210, 
+      child: Card(
+        margin: const EdgeInsets.only(left: 20),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18, top: 15, bottom: 10, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 0),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 132, 105, 251),
                 ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    // Add to cart action
-                  },
-                  child: const Text('Add to cart'),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                author,
+                style: const TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
                 ),
-              ],
-            ),
-          ],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 10), 
+              ClipRRect(
+                borderRadius:
+                    BorderRadius.circular(8), 
+                child: Image.asset(
+                  coverImagePath,
+                  width: 140,
+                  height: 200,
+                  fit: BoxFit
+                      .cover, 
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: Text(
+                      'Rs. $price',
+                      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                    ),
+                    child: const Text('Add to cart'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
